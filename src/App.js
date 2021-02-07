421e1856c9d81340590176e0b703729637d9d1ce
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-02-07 20:44:25 -0300

    inicia o projeto cronometro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import React, { Component } from "react";
+import imgCronometro from "./assets/cronometro.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <img src={imgCronometro} alt="cronometro" className="img" />
+        <span className="timer">0:00</span>
+        <div className="button-container">
+          <a href="/vai" className="btn btn-go">
+            vai
+          </a>
+          <a href="/zerar" className="btn btn-reset">
+            Zerar
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
