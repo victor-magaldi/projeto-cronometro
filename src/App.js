73ea82ceea2810f2dfac0414a9046ab52cb3d47b
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-02-09 19:46:14 -0300

    customiza os btns

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React, { Component } from "react";
 import imgCronometro from "./assets/cronometro.png";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { numero: 0 };
+  }
   render() {
     return (
       <div className="container">
@@ -10,7 +14,7 @@ export default class App extends Component {
         <span className="timer">0:00</span>
         <div className="button-container">
           <a href="/vai" className="btn btn-go">
-            vai
+            Vai
           </a>
           <a href="/zerar" className="btn btn-reset">
             Zerar
