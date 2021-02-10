9a459fd03a96d35ae0469780ae3fe5226a0f788c
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-02-09 21:59:12 -0300

    conclui lógica timer

--- src/App.js
+++ src/App.js
@@ -5,18 +5,48 @@ import imgCronometro from "./assets/cronometro.png";
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { numero: 0 };
+    this.state = { numero: 0, btn: "Vai" };
+
+    this.timer = null;
+
+    this.vai = this.vai.bind(this);
+    this.zerar = this.zerar.bind(this);
+  }
+
+  vai(evt) {
+    evt.preventDefault();
+
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+      this.setState({ btn: "Vai" });
+    } else {
+      this.timer = setInterval(() => {
+        let state = this.state;
+        state.numero += 0.1;
+
+        this.setState({ numero: state.numero, btn: "Parar" });
+      }, 100);
+    }
+  }
+  zerar(evt) {
+    evt.preventDefault();
+
+    clearInterval(this.timer);
+    this.timer = null;
+    this.setState({ numero: 0, btn: "Vai" });
   }
   render() {
     return (
       <div className="container">
         <img src={imgCronometro} alt="cronometro" className="img" />
-        <span className="timer">0:00</span>
+        <span className="timer">{this.state.numero.toFixed(1)}</span>
         <div className="button-container">
-          <a href="/vai" className="btn btn-go">
-            Vai
+          <a href="/#" className="btn btn-go" onClick={this.vai}>
+            {this.state.btn}
           </a>
-          <a href="/zerar" className="btn btn-reset">
+
+          <a href="/#" className="btn btn-reset" onClick={this.zerar}>
             Zerar
           </a>
         </div>
